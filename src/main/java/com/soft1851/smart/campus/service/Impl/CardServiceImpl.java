c22ecbe247a7eeffd4e0579286f991d9c5d19a4e
zhent1106 <1299088269@qq.com>
2020-05-29 23:19:06 +0800

    一卡通增加、分页查询、删除

---
+++ src/main/java/com/soft1851/smart/campus/service/Impl/CardServiceImpl.java
@@ -0,0 +1,51 @@
+package com.soft1851.smart.campus.service.Impl;
+
+import com.soft1851.smart.campus.constant.ResponseResult;
+import com.soft1851.smart.campus.model.dto.PageDto;
+import com.soft1851.smart.campus.model.entity.SysCard;
+import com.soft1851.smart.campus.repository.CardRepository;
+import com.soft1851.smart.campus.service.CardService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName CardServiceImpl
+ * @Description TODO
+ * @Author 田震
+ * @Date 2020/5/26
+ **/
+@Service
+public class CardServiceImpl implements CardService {
+    @Resource
+   private CardRepository cardRepository;
+
+    @Override
+    public ResponseResult findAllByPage(PageDto pageDto) {
+        Pageable pageable = PageRequest.of(
+                pageDto.getCurrentPage(),
+                pageDto.getPageSize(),
+                Sort.Direction.ASC,
+                "pkCardId");
+        Page<SysCard> sysCards = cardRepository.findAll(pageable);
+        return ResponseResult.success(sysCards.getContent());
+    }
+
+    @Override
+    public ResponseResult insertAll(List<SysCard> sysCards) {
+        List<SysCard> sysCardList=new ArrayList<>();
+        return ResponseResult.success(sysCardList);
+    }
+
+    @Override
+    public ResponseResult deleteCard(Long pkCardId) {
+        int a=cardRepository.delete(pkCardId);
+        return ResponseResult.success(a);
+    }
+}
