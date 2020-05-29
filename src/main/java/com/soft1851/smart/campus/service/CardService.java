8fbcf7afdad70b10d7a3ddf340b8a78884a246c1
zhent1106 <1299088269@qq.com>
2020-05-29 23:22:57 +0800

    一卡通查询、批量增加

---
+++ src/main/java/com/soft1851/smart/campus/service/CardService.java
@@ -0,0 +1,36 @@
+package com.soft1851.smart.campus.service;
+
+
+import com.soft1851.smart.campus.constant.ResponseResult;
+import com.soft1851.smart.campus.model.dto.PageDto;
+import com.soft1851.smart.campus.model.entity.SysCard;
+
+import java.util.List;
+
+/**
+ * @ClassName CardService
+ * @Description TODO
+ * @Author 田震
+ * @Date 2020/5/26
+ **/
+public interface CardService {
+    /**
+     * 分页查询一卡通数据
+     * @param pageDto
+     * @return
+     */
+    ResponseResult findAllByPage(PageDto pageDto);
+
+    /**
+     * 批量增加一卡通信息
+     * @return
+     * @param sysCards
+     */
+    ResponseResult insertAll(List<SysCard> sysCards);
+
+    /**
+     * 删除一卡通信息
+     * @return
+     */
+    ResponseResult deleteCard(Long pkCardId);
+}
--- src/test/java/com/soft1851/smart/campus/service/RedisServiceTest.java
+++ src/test/java/com/soft1851/smart/campus/service/RedisServiceTest.java
@@ -5,14 +5,28 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 /**
  * @Description TODO
  * @Author wf
- * @Date 2020/5/26
+ * @Date 2020/5/29
  * @Version 1.0
  */
 @SpringBootTest
 class RedisServiceTest {
+    @Resource
+    private RedisService redisService;
 
+    @Test
+    void set() {
+        redisService.set("1","112221", (long)1);
 
+    }
+
+    @Test
+    void getValue() {
+        System.out.println(redisService.getValue("1", String.class));
+
+    }
 }
