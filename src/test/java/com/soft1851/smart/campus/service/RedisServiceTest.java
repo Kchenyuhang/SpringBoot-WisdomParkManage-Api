a8e8ac635ec72e86df86b03e6c14c88f38e22fb8
wangfeng <1244353765@qq.com>
2020-05-29 19:09:15 +0800

    修改框架启动不起来的问题

--- src/test/java/com/soft1851/smart/campus/service/RedisServiceTest.java
+++ src/test/java/com/soft1851/smart/campus/service/RedisServiceTest.java
@@ -1,16 +1,32 @@
 package com.soft1851.smart.campus.service;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.Resource;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @Description TODO
  * @Author wf
- * @Date 2020/5/26
+ * @Date 2020/5/29
  * @Version 1.0
  */
 @SpringBootTest
 class RedisServiceTest {
+    @Resource
+    private RedisService redisService;
 
+    @Test
+    void set() {
+        redisService.set("1","112221", (long)1);
 
+    }
+
+    @Test
+    void getValue() {
+        System.out.println(redisService.getValue("1", String.class));
+
+    }
 }
