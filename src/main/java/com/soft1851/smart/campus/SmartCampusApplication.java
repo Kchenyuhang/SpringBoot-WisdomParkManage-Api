584d9cc9346bc48b1c5b0d59f2fc60fa5b0cb539
wangfeng <1244353765@qq.com>
2020-06-02 00:26:43 +0800

    楼栋管理模块接口

--- src/main/java/com/soft1851/smart/campus/SmartCampusApplication.java
+++ src/main/java/com/soft1851/smart/campus/SmartCampusApplication.java
@@ -2,11 +2,13 @@ package com.soft1851.smart.campus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
  * @author
  */
 @SpringBootApplication
+@EnableJpaAuditing
 public class SmartCampusApplication {
 
     public static void main(String[] args) {
