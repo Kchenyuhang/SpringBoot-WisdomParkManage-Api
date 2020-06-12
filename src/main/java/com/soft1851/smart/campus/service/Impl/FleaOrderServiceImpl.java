8f8658b0bd84b1b7e8e750ab7613d04f97bf6a76
tyx <1427177855@qq.com>
2020-06-12 11:06:58 +0800

    提交

--- src/main/java/com/soft1851/smart/campus/service/Impl/FleaOrderServiceImpl.java
+++ src/main/java/com/soft1851/smart/campus/service/Impl/FleaOrderServiceImpl.java
@@ -3,12 +3,14 @@ package com.soft1851.smart.campus.service.Impl;
 import com.soft1851.smart.campus.constant.ResponseResult;
 import com.soft1851.smart.campus.model.dto.FleaOrderDto;
 import com.soft1851.smart.campus.service.FleaOrderService;
+import org.springframework.stereotype.Service;
 
 /**
  * @Author yhChen
  * @Description
  * @Date 2020/6/12
  */
+@Service
 public class FleaOrderServiceImpl implements FleaOrderService {
     @Override
     public ResponseResult orderIncreased(FleaOrderDto fleaOrderDto) {
