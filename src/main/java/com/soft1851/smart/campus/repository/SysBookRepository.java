cef2f4078f67cfee78fa9672b892e04e62437174
tyx <1427177855@qq.com>
2020-06-13 13:59:20 +0800

    提交

--- src/main/java/com/soft1851/smart/campus/repository/SysBookRepository.java
+++ src/main/java/com/soft1851/smart/campus/repository/SysBookRepository.java
@@ -1,8 +1,11 @@
 package com.soft1851.smart.campus.repository;
 
+import com.soft1851.smart.campus.model.entity.SysBook;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
  * @author Tao
  */
-public interface SysBookRepository {
+public interface SysBookRepository extends JpaRepository<SysBook, Long> {
 
 }
